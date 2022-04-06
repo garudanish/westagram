04ca7768c68ec34458b1b47fc42a545ae63133a7
Hyeonsu Kim <hyeonsu.kim.dev@gmail.com>
2022-04-06 10:24:43 +0900

    추가 기능: 검색 기능 구현

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ const bookmarkButton = document.body.querySelector(".button-bookmark");
 const postCommentInput = document.body.querySelector(".post-comment-input");
 const postCommentButton = document.body.querySelector(".post-comment-button");
 const postCommentList = document.body.querySelector(".post-comment-list");
+const gnbSearchBar = document.body.querySelector(".gnb-search");
+const gnbSearchResult = document.body.querySelector(".gnb-search-result");
+const gnbSearchResultList = document.body.querySelector(
+  ".gnb-search-result-list"
+);
 
 // 피드 버튼들 동작 시 클래스 fa-solid 바꾸기 등
 
@@ -26,6 +31,8 @@ postCommentInput.addEventListener("keyup", () => {
 });
 
 // 댓글 입력하고 enter 프레스 하면 댓글 달리게 하기
+
+// 1. li 엘리먼트를 만드는 함수
 const createCommentLi = (id, comment) => {
   const li = document.createElement("li");
   li.className = "post-comment-item";
@@ -81,6 +88,8 @@ const createCommentLi = (id, comment) => {
   return li;
 };
 
+// 2-1. 버튼 클릭 시 댓글 추가
+
 postCommentButton.addEventListener("click", () => {
   if (!postCommentInput.value.slice().trim()) {
     return;
@@ -92,6 +101,8 @@ postCommentButton.addEventListener("click", () => {
   postCommentList.appendChild(newComment);
 });
 
+// 2-2. 엔터 입력 시 댓글 추가
+
 postCommentInput.addEventListener("keyup", (e) => {
   if (e.code === "Enter") {
     if (!postCommentInput.value.slice().trim()) {
@@ -102,4 +113,92 @@ postCommentInput.addEventListener("keyup", (e) => {
     postCommentButton.disabled = true;
     postCommentList.appendChild(newComment);
   }
+});
+
+// 아이디 검색 기능
+
+const idDataArray = [
+  {
+    id: "wecode_bootcamp",
+    username: ">wecode | 위코드",
+    profileImgUrl:
+      "https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/53930194_2310969732301384_5134167175078084608_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=2QAs8zwSlP0AX9Yv2f9&_nc_ht=scontent-ssn1-1.xx&oh=00_AT9koDC1iUJjCHPBquxVXhGHmIKWITRsl7teV5B8J9xCUA&oe=62732DB2",
+  },
+  {
+    id: "sfc.seongnam",
+    username: "성남FC - Seongnam FC",
+    profileImgUrl:
+      "https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/52606079_1182941335204372_5780145288782020608_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=8eSGiwkmhv8AX_ZAVko&_nc_ht=scontent-ssn1-1.xx&oh=00_AT9mf9yHtSNjSQ7yHFvBWYafvkBDAwdUo0vPmrvRFngXaw&oe=6272465A",
+  },
+  {
+    id: "suwonsamsungfc",
+    username: "수원삼성블루윙즈축구단",
+    profileImgUrl:
+      "https://scontent-ssn1-1.xx.fbcdn.net/v/t1.6435-9/179942605_3973581779374288_4019617340567091806_n.jpg?_nc_cat=103&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=aLb1iZ9O798AX8TnE-S&tn=Af7c0gtskNonnRcz&_nc_ht=scontent-ssn1-1.xx&oh=00_AT9dx38wVLtSitl48yjLn1fePmYzpoPPcacR3oDTOyviLQ&oe=62729187",
+  },
+  {
+    id: "suwonfc",
+    username: "수원FC (Suwon FC)",
+    profileImgUrl:
+      "https://scontent-ssn1-1.xx.fbcdn.net/v/t39.30808-6/241263193_5993106480763304_6447924619962113569_n.jpg?_nc_cat=102&ccb=1-5&_nc_sid=09cbfe&_nc_ohc=opmy1_zR0D8AX_uMgfy&tn=Af7c0gtskNonnRcz&_nc_ht=scontent-ssn1-1.xx&oh=00_AT_iMCZnOSt8Z7TQBwUGB41pdsEuznNioQFYfuMpXa37Lw&oe=625045FD",
+  },
+];
+
+let matchedArray = idDataArray;
+
+const paintMatchedArray = () => {
+  gnbSearchResultList.innerHTML = "";
+
+  matchedArray.map((el) => {
+    const matchedLi = document.createElement("li");
+    matchedLi.className = "gnb-search-result-item";
+
+    const matchedLiLeft = document.createElement("div");
+    matchedLiLeft.className = "gnb-search-result-item-left";
+    const profileImg = document.createElement("img");
+    profileImg.src = el.profileImgUrl;
+    profileImg.className = "gnb-search-result-item-img";
+    const idUsernameWrapper = document.createElement("div");
+    idUsernameWrapper.className = "gnb-search-result-item-profile";
+    const id = document.createElement("strong");
+    id.textContent = el.id;
+    id.className = "gnb-search-result-item-id";
+    const username = document.createElement("span");
+    username.textContent = el.username;
+    username.className = "gnb-search-result-item-username";
+
+    idUsernameWrapper.appendChild(id);
+    idUsernameWrapper.appendChild(username);
+
+    matchedLiLeft.appendChild(profileImg);
+    matchedLiLeft.appendChild(idUsernameWrapper);
+
+    const matchedLiRight = document.createElement("div");
+    matchedLiRight.className = "gnb-search-result-item-right";
+    const xButton = document.createElement("i");
+    xButton.className = "fa-solid fa-x";
+    matchedLiRight.appendChild(xButton);
+
+    matchedLi.appendChild(matchedLiLeft);
+    matchedLi.appendChild(matchedLiRight);
+
+    gnbSearchResultList.appendChild(matchedLi);
+  });
+};
+
+gnbSearchBar.addEventListener("focus", () => {
+  gnbSearchResult.classList.add("is-active");
+  paintMatchedArray();
+});
+
+gnbSearchBar.addEventListener("blur", () => {
+  gnbSearchResult.classList.remove("is-active");
+});
+
+gnbSearchBar.addEventListener("keyup", (e) => {
+  matchedArray = idDataArray.filter((el) =>
+    el.id.startsWith(e.target.value.toLowerCase())
+  );
+
+  paintMatchedArray();
 });
